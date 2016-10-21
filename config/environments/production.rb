015bb0de9a6a19963c324e1ac9ba61e132f08ca5
daspearsman <spears.alexj@gmail.com>
2016-10-21 01:41:55 +0000

    Add changes to email template production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000'
+  host = 'fierce-tor-26360.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
